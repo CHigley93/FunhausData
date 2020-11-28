5a9a71ceab55fbf953750958cb1f5b8cd41bcfb4
CHigley93 <50812667+CHigley93@users.noreply.github.com>
2020-11-28 14:30:54 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+import pandas as pd
+import json
+
+
+def auth():
+    return "AAAAAAAAAAAAAAAAAAAAAClXKAEAAAAAy8LisO8SDlbC%2B9dTwn06D2spd2k%3DYOTZjkEancpjcbOO86z7JGLG21kLtYNIADUypT9A1V4OK7rPgC"
+
+
+def create_url(names):
+    usernames = "usernames=" + names
+    user_fields = "user.fields=description,created_at,public_metrics,username"
+    url = "https://api.twitter.com/2/users/by?{}&{}".format(usernames, user_fields)
+    return url
+
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+
+def connect_to_endpoint(url, headers):
+    response = requests.request("GET", url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+
+funhaus_list = ["FunHausTeam", "ElyseWillems", "JamesWillems", "SirLarr", "brucegreene", "jonsmiff", "charalanahzard"]
+formatted_list = []
+bearer_token = auth()
+for name in funhaus_list:
+    url = create_url(name)
+    headers = create_headers(bearer_token)
+    json_response = connect_to_endpoint(url, headers)
+    formatted_list.append([json_response.get("data")[0].get("name"),
+                           json_response.get("data")[0].get("public_metrics").get("followers_count"),
+                           json_response.get("data")[0].get("public_metrics").get("tweet_count")])
+
+data = pd.DataFrame(formatted_list)
+data.columns = ["Name", "Followers", "Tweets"]
+data.to_csv("Funhaus Twitter Data.csv", index=False)
